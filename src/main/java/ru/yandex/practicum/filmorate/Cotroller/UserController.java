825daf55c7907302d8719d23a0ba210f106494c1
Дмитрий <d.a.korovin@yandex.ru>
2023-04-30 12:07:38 +0300

    add Controllers and Validation

---
+++ src/main/java/ru/yandex/practicum/filmorate/Cotroller/UserController.java
@@ -0,0 +1,7 @@
+package Cotroller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class UserController {
+}
