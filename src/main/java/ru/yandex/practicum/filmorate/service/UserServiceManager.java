8db633e26952302e6900abda9b590853b1477368
Дмитрий <d.a.korovin@yandex.ru>
2023-05-14 10:27:28 +0300

    codestyle

--- src/main/java/ru/yandex/practicum/filmorate/service/UserServiceManager.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserServiceManager.java
@@ -9,9 +9,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
